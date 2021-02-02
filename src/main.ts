fb4ce4ed63c4868a1e1d1ee4b93e38f4b93553bf
jiangyi <jiangyi@bertadata.com>
2021-02-02 14:10:22 +0800

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './app.vue'
 import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Router from './router'
+import Store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(Store).use(Router).mount('#app')
