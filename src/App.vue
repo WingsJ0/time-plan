796eb3066022ebca628813ac80ecd919d4a1b6b9
jiangyi <jiangyi@bertadata.com>
2021-02-05 13:56:02 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <keep-alive>
+    <router-view />
+  </keep-alive>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  * {
+    box-sizing: border-box;
   }
-}
+  html {
+    color: #333333;
+    font-size: 14px;
+    font-family: 'Microsoft Jhenghei', 'Microsoft Yahei', sans-serif;
+  }
+  body {
+    overflow: hidden;
+    width: 100vw;
+    height: 100vh;
+    padding: 0;
+    margin: 0;
+  }
 </style>
---
+++ src/views/layout/index.vue
@@ -0,0 +1,13 @@
+<!--
+@name
+-->
+
+<template>
+  <div class="layout">
+    <div class="directory"></div>
+    <div class="wrap"></div>
+  </div>
+</template>
+
+<script src="./component.ts" lang="ts"></script>
+<style src="./style.scss" lang="scss" scoped></style>
